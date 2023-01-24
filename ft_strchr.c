e4555233ebec1fc2eb722dd9d478958bb1e19213
Yudai Shimoma <yshimoma@c1r11s5.42tokyo.jp>
2023-01-24 21:27:35 +0900

    unit-test2での修正

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,34 +6,43 @@
 /*   By: yshimoma <yshimoma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 18:33:16 by yshimoma          #+#    #+#             */
-/*   Updated: 2023/01/23 17:21:42 by yshimoma         ###   ########.fr       */
+/*   Updated: 2023/01/24 20:48:50 by yshimoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-// #include <stdio.h>
-// #include <string.h>
+
+// /*
+// ** 与えられた文字列の桁数を返す
+// */
+// size_t	ft_strlen(const char *s)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	while (s[i] != '\0')
+// 		i++;
+// 	return (i);
+// }
 
 /*
 ** 文字列の先頭から「文字」を検索して見つかった場所をポインタで返します。
 */
 char	*ft_strchr(const char *s, int c)
 {
-	if (c == 0)
-		return ("\0");
-	if (c > 255)
-		c -= 256;
-	if (c < 0)
-		c += 256;
 	while (*s != '\0')
 	{
 		if (*s == (char)c)
 			return ((char *)s);
 		s++;
 	}
-	return (0);
+	if (c == 0)
+		return ((char *)s);
+	return (NULL);
 }
 
+// #include <stdio.h>
+// #include <string.h>
 // int	main(void)
 // {
 // 	// char	test[20] = "abcdefghijk";
@@ -45,21 +54,26 @@ char	*ft_strchr(const char *s, int c)
 // 	// x = ft_strchr((const char *)test, 'f');
 // 	// printf("検索文字が見つかった場所から表示→%s\n", x);
 
-// 	char *s = "libft-test-tokyo";
+// 	// char *s = "libft-test-tokyo";
 
-	// /* 17 */ printf("17strchrで検索文字が見つかった場所から表示→%s\n", strchr(s, '\0'));
-// /* 17 */ printf("17ft_strchrで検索文字が見つかった場所から表示→%s\n\n", ft_strchr(s, '\0'));
-	// /* 18 */ printf("18strchrで検索文字が見つかった場所から表示→%s\n", strchr(s, 'l' + 256));
-	// printf("18ft_strchrで検索文字が見つかった場所から表示→%s\n\n", ft_strchr(s, 'l' + 256));
-	// /* 19 */ printf("18strchrで検索文字が見つかった場所から表示→%s\n", strchr(s, 'i' + 256));
-	// printf("18ft_strchrで検索文字が見つかった場所から表示→%s\n\n", ft_strchr(s, 'i' + 256));
-	// /* 20 */ printf("18strchrで検索文字が見つかった場所から表示→%s\n", strchr(s, 'l' - 256));
-	// printf("18ft_strchrで検索文字が見つかった場所から表示→%s\n\n", ft_strchr(s, 'l' - 256));
-	// /* 21 */ printf("18strchrで検索文字が見つかった場所から表示→%s\n", strchr(s, 'i' - 256));
-	// printf("18ft_strchrで検索文字が見つかった場所から表示→%s\n\n", ft_strchr(s, 'i' - 256));
-// 	/* 22 */ printf("18strchrで検索文字が見つかった場所から表示→%s\n", strchr(s, 'z'));
-// 	printf("18ft_strchrで検索文字が見つかった場所から表示→%s\n\n", ft_strchr(s, 'z'));
-// 	/* 23 */ printf("18strchrで検索文字が見つかった場所から表示→%s\n", strchr(s, '~'));
-// 	printf("18ft_strchrで検索文字が見つかった場所から表示→%s\n\n", ft_strchr(s, '~'));
+// 	// /* 17 */ printf("17strchrで検索文字が見つかった場所から表示→%s\n", strchr(s, '\0'));
+// //   printf("17ft_strchrで検索文字が見つかった場所から表示→%s\n\n", ft_strchr(s, '\0'));
+// 	// /* 18 */ printf("18strchrで検索文字が見つかった場所から表示→%s\n", strchr(s, 'l' + 256));
+// 	// printf("18ft_strchrで検索文字が見つかった場所から表示→%s\n\n", ft_strchr(s, 'l' + 256));
+// 	// /* 19 */ printf("18strchrで検索文字が見つかった場所から表示→%s\n", strchr(s, 'i' + 256));
+// 	// printf("18ft_strchrで検索文字が見つかった場所から表示→%s\n\n", ft_strchr(s, 'i' + 256));
+// 	// /* 20 */ printf("18strchrで検索文字が見つかった場所から表示→%s\n", strchr(s, 'l' - 256));
+// 	// printf("18ft_strchrで検索文字が見つかった場所から表示→%s\n\n", ft_strchr(s, 'l' - 256));
+// // 	/* 21 */ printf("18strchrで検索文字が見つかった場所から表示→%s\n", strchr(s, 'i' - 256));
+// // 	printf("18ft_strchrで検索文字が見つかった場所から表示→%s\n\n", ft_strchr(s, 'i' - 256));
+// // 	/* 22 */ printf("18strchrで検索文字が見つかった場所から表示→%s\n", strchr(s, 'z'));
+// // 	printf("18ft_strchrで検索文字が見つかった場所から表示→%s\n\n", ft_strchr(s, 'z'));
+// // 	/* 23 */ printf("18strchrで検索文字が見つかった場所から表示→%s\n", strchr(s, '~'));
+// // 	printf("18ft_strchrで検索文字が見つかった場所から表示→%s\n\n", ft_strchr(s, '~'));
+
+// 	char *src = "there is so \0ma\0ny \0 \\0 in t\0his stri\0ng !\0\0\0\0";
+// 	printf("%s\n", strchr(src, '\0'));
+// 	printf("%s\n", ft_strchr(src, '\0'));
+
 // 	return (0);
 // }
